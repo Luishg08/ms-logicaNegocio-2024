4273b68c6d2e5575d77f5df436d49189b30d8e6e
Luis Henao García <luis.henao37085@ucaldas.edu.co>
2024-03-17 12:45:29 -0500

    Integración de las Estrategias de Autorización - Parte 1

---
+++ src/config/configuracion.seguridad.ts
@@ -0,0 +1,9 @@
+export namespace ConfiguracionSeguridad {
+  export const menuProductoId = "65d9247c971b150fdc22502b";
+  export const listarAccion = "listar"
+  export const editarAccion = "editar"
+  export const eliminarAccion = "eliminar"
+  export const guardarAccion = "guardar"
+  export const descargarAccion = "descargar"
+
+}
